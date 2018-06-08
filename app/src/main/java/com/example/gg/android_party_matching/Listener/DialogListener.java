18be02da0fec3849158b39c2b5f0a8c1b3f8c6da
kwonhanJun <skclaqks11@naver.com>
2018-06-08 18:10:39 +0900

    회원가입 다이얼로그 완료

---
+++ app/src/main/java/com/example/gg/android_party_matching/Listener/DialogListener.java
@@ -0,0 +1,6 @@
+package com.example.gg.android_party_matching.Listener;
+
+// Activity와 DialogFragment간의 데이터를 주고 받기 위해 리스너를 만든다
+public interface DialogInterface {
+    void onFinishEditingDialog(String inputText);
+}
