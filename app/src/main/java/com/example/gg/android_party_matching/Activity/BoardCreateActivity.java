d7f3c5f1aed7feaeaaa815e21adff7a5a4cd39bf
kwonhanJun <skclaqks11@naver.com>
2018-06-24 16:32:01 +0900

    add BoardCreateActivity.java

---
+++ app/src/main/java/com/example/gg/android_party_matching/Activity/BoardCreateActivity.java
@@ -0,0 +1,15 @@
+package com.example.gg.android_party_matching.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gg.android_party_matching.R;
+
+public class BoardCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_create);
+    }
+}
--- app/src/main/java/com/example/gg/android_party_matching/Fragment/BoardListFragment.java
+++ app/src/main/java/com/example/gg/android_party_matching/Fragment/BoardListFragment.java
@@ -1,8 +1,10 @@
 package com.example.gg.android_party_matching.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,13 +12,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.gg.android_party_matching.Activity.BoardCreateActivity;
+import com.example.gg.android_party_matching.Activity.BoardTabActivity;
 import com.example.gg.android_party_matching.RecyclerView.BoardRVAdapter;
 import com.example.gg.android_party_matching.R;
 
-public class BoardListFragment extends Fragment {
+public class BoardListFragment extends Fragment implements View.OnClickListener {
     private RecyclerView         mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton floatingActionButton;
 
     @Nullable
     @Override
@@ -31,7 +36,23 @@ public class BoardListFragment extends Fragment {
         mAdapter = new BoardRVAdapter(getActivity());
         mRecyclerView.setAdapter(mAdapter);
 
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fabtnBoardAdd);
+        floatingActionButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.fabtnBoardAdd:
+                Intent intent = new Intent(getContext(), BoardCreateActivity.class);
+                startActivity(intent);
+                break;
+                default:
+                    break;
+
+        }
     }
 
 
